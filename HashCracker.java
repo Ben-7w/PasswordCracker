35602bd37892dfbe730db9bd50042c4a8ce741a4
Ben <tolissodiop@gmail.com>
2026-07-16 00:08:15 +0000

    Nettoyage de la racine pour la restructuration en packages

--- HashCracker.java
+++
@@ -1,8 +0,0 @@
-public interface HashCracker {
-    /**
-     * Tente de retrouver le mot de passe à partir de son hash.
-     * @param hash L'empreinte MD5 à casser.
-     * @return Le mot de passe en clair s'il est trouvé, sinon null.
-     */
-    String crack(String hash);
-}
